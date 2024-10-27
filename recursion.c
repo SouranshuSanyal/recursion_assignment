2ec147a31d00bc393cd6139c67064d224a14db61
Souranshu <souranshus@gmail.com>
2024-10-27 11:26:00 +0530

    changed

--- recursion.c
+++ recursion.c
@@ -25,23 +25,23 @@
 //Q4
 // #include <stdio.h>
 
-// int sum(int y){
-//     int temp=0;
-//     if (y==0){
-//         return 0;
-//     }
-//     else{
-//         temp = sum(y/10) + y%10;
-//     }
-//     return temp;
-// }
+int sum(int y){
+    int temp=0;
+    if (y==0){
+        return 0;
+    }
+    else{
+        temp = sum(y/10) + y%10;
+    }
+    return temp;
+}
 
-// void main(){
-//     int x;
-//     printf ("Enter Your Number: ");
-//     scanf("%d", &x);
+void main(){
+    int x;
+    printf ("Enter Your Number: ");
+    scanf("%d", &x);
 
-//     int s = sum(x);
+    int s = sum(x);
 
-//     printf ("Your sum of digits are: %d", s);
-// }
+    printf ("Your sum of digits are: %d", s);
+}
